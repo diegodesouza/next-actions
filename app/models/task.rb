3d343100835479dffb0d869c00213c7c7c539f94
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-01 23:03:36 -0500

    some changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ActiveRecord::Base
+  belongs_to :user,
+  inverse_of: :tasks
+
+  validates_presence_of :title
+  validates_presence_of :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102034420) do
+ActiveRecord::Schema.define(version: 20150102035741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150102034420) do
     t.string   "title",       null: false
     t.text     "description"
     t.integer  "user_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
